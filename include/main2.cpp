9347c122ec9ba0ae975652d3fe8b7a4bd1a6eba2
Juan Camilo Reyes <j,reyesc@uniandes.edu.co>
2023-06-01 11:32:06 -0500

    comentarios

--- include/main2.cpp
+++ include/main2.cpp
@@ -1,31 +1,30 @@
 #include <Wire.h>
 #define Dir  19    // pin DIR de A4988 a pin 5
 #define Step  18     // pin STEP de A4988 a pin 4
-#define enable 5
-#define encoder_pin 34
-int iteraciones = 0;
-int iteraciones2 = 0;
-int rpm_deseado = 100;
-int rpm_prom;
-unsigned long time_old;
+#define enable 5    // pin ENABLE de A4988 a pin 3
+#define encoder_pin 34  // pin for edge detection
+int iteraciones = 0; //num of iterations for correction
+int iteraciones2 = 0; //num of iterations for rpm average
+int rpm_deseado = 100; // rpm desired
+int rpm_prom; // rpm average
+unsigned long time_old; // time for rpm
 unsigned int pulses_per_turn = 20; // Depends on the number of spokes on the encoder wheel
-bool flag = false;
-int Encoder = 0;
-int Estado = 1;
-int Pulsos = 0;
-int count = 0;
-int PulsosRotacion = 20;
-int del = 0;
-int new_del = 1400;
-unsigned long InitialTime = 0;
-unsigned long FinalTime;
-float RPMs;
-int recibido = 0;
-bool reci = false;
-int rpm_print = 0;
+bool flag = false; // flag for correction
+int Encoder = 0; // first variable used to save the state of the encoder pin
+int Estado = 1; // second variable used to save the state of the encoder pin
+int Pulsos = 0; // Variable used to save the number of pulses
+int PulsosRotacion = 20; // Number of pulses per rotation
+int del = 0; // delay for stepper
+int new_del = 1400; // new delay for stepper
+unsigned long InitialTime = 0; // Variable used to save the initial time
+unsigned long FinalTime; // Variable used to save the final time
+float RPMs; // Variable used to save the RPMs
+int recibido = 0; // Data received by I2C
+bool reci = false; // If data was received by I2C
+int rpm_print = 0; // rpm to print
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Esta funcion es para mover el stepper. Se mandan impulsos usando el pin step. El tiempo que transcurre entre impulsos se define con new_del 
-// la cual depende del rpm deseado. 
+// This function is for moving the stepper. Pulses are sent using the step pin. The time between pulses is defined by new_del
+// which depends on the desired RPM.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void mover(int new_del){
@@ -38,8 +37,8 @@ void mover(int new_del){
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Funcion para medir el rpm actual. Usa los pines de deteccion del encoder. Como el encoder es optico, los impulsos de entrada indica que
-// se avanzo un paso.
+// Function to measure the current RPM. It uses the encoder detection pins. Since the encoder is optical, the incoming pulses indicate that
+// a step has been taken..
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void medir_rpm(){
   Encoder = digitalRead(encoder_pin);
@@ -53,34 +52,34 @@ void medir_rpm(){
   }
   if (Encoder == HIGH && Estado == 0)
   {
-    Pulsos++; // Si hubo un flanco completo de subida, se incrementa en 1 el numero de pulsos que se han dado
+    Pulsos++; // If there was a complete rising edge, increment the pulse count by 1
     Estado = 1;
   }
-  if (Pulsos == PulsosRotacion) // Si el numero de pulsos es equivalente a una vuelta, se mide cuanto tiempo se tardo en dar la vuelta
+  if (Pulsos == PulsosRotacion) // If the number of pulses is equivalent to one rotation, measure the time it took to complete the rotation
   {
-    FinalTime = millis(); // Se toma el tiempo final
-    RPMs = 60000 / (FinalTime - InitialTime); // Se calculan los rpms
+    FinalTime = millis(); // Take the final time
+    RPMs = 60000 / (FinalTime - InitialTime); // Calculate the RPMs
     rpm_prom = rpm_prom + RPMs; 
     Pulsos = 0;
     InitialTime = FinalTime;
     iteraciones2++;
-    if (iteraciones2 == 4) { // Se calcula el rpm promedio de las ultimas 4 iteraicones
-      rpm_print = rpm_prom/4; // El promedio es el rpm medido que se va a mandar a la otra esp
+    if (iteraciones2 == 4) { // Calculate the average RPM of the last 4 iterations
+      rpm_print = rpm_prom/4; // The average is the measured RPM to be sent to the other esp device
       iteraciones2= 0;
       rpm_prom=0;
       } 
   }
   }
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Funcion para corregir el valor del delay en caso de que no se haya llegado al rpm deseado. Si el rpm es mayor al deseado, se debe aumentar
-// el delay, si es menor se debe aumentar. 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Function to correct the value of the delay in case the desired RPM has not been reached. If the RPM is higher than the desired value,
+// the delay should be increased; if it is lower, the delay should be decreased.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void corregir(int rpm_deseado){
-    if (((rpm_deseado - 1 <= RPMs) || (rpm_deseado + 1 >= RPMs) ) && (iteraciones == 5)) { // La correccion se hace cada 5 iteraciones para dejar que el sistema se estabilice
+    if (((rpm_deseado - 1 <= RPMs) || (rpm_deseado + 1 >= RPMs) ) && (iteraciones == 5)) { // The correction is done every 5 iterations to allow the system to stabilize.
     if (rpm_deseado == RPMs){
       new_del = new_del;
-      flag = true; // Se llego al deseado, mantener el delay en ese valor
+      flag = true; // If the desired RPM has been reached, keep the delay at that value.
       }   
     else if ((rpm_deseado > RPMs)&& (flag == false)) {
       new_del = new_del-1;
@@ -93,7 +92,7 @@ void corregir(int rpm_deseado){
   iteraciones ++;
   }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Recibir por i2c el valor de rpm deseado. Este valor se manda desde la esp con el codigo main
+// Receive the desired RPM value via I2C. This value is sent from the main code on the other device (ESP)
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void receiveEvent(int numBytes) {
@@ -110,8 +109,8 @@ void receiveEvent(int numBytes) {
   }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Para mandar el valor de rpm actual. La solicitud del dato se hace en el loop del main y esta funcion se ejecuta siempre que se solicite
-////////////////////////////////////////////////////////////////////////////////////////
+// To send the current RPM value, the request for the data is made in the main loop, and this function is executed whenever it is requested.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void requestEvent() {
   uint8_t highByte = ( rpm_print >> 8) & 0xFF; // Se manda elvalor actual del rpm
   uint8_t lowByte = rpm_print & 0xFF;
@@ -121,14 +120,14 @@ void requestEvent() {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
-  Wire.begin(0x23);                // Esta es la direccion i2c slave de esta esp
-  Wire.onReceive(receiveEvent); // cada vez que reciba un dato
-  Wire.onRequest(requestEvent); // Esta funcion se activa cada vez que main le solicite el valor actual del rpm
+  Wire.begin(0x23);                // This is the I2C slave address of this device
+  Wire.onReceive(receiveEvent); // Triggered whenever data is received
+  Wire.onRequest(requestEvent); // This function is activated whenever the main code requests 
   //Serial.begin(9600);
   //Serial.println ("Inicia");
-  pinMode(Step, OUTPUT);  // Pin para mandar 1 o 0 para hacer los pasos del stepper
-  pinMode(Dir, OUTPUT);  // Pin de direccion del stepper
-  pinMode(encoder_pin, INPUT); // Pin de deteccion de flancos del encoder
+  pinMode(Step, OUTPUT);  // Pin for sending 1 or 0 to perform stepper motor steps
+  pinMode(Dir, OUTPUT);  // Stepper motor direction pin
+  pinMode(encoder_pin, INPUT); // Encoder edge detection pin
   pinMode(enable, OUTPUT);
   digitalWrite(enable, LOW);
   }
@@ -136,6 +135,8 @@ void setup() {
 
  
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Loop of functios that are executed continuously
 void loop() {
 mover(new_del);
 medir_rpm(); 
